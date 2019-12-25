9bb09dc32c9b994195281e46e3e4aedd6d2dd29f
AjibsBaba <samuelajibade22@gmail.com>
2019-12-25 08:45:58 +0100

    Setting deployment settings

--- Emotions/settings.py
+++ Emotions/settings.py
@@ -120,17 +120,17 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static"),
 ]
 STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 WSGI_APPLICATION = 'Emotions.wsgi.application'
 ASGI_APPLICATION = "Emotions.routine.application"
 
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
 
 # SECURITY
 CSRF_COOKIE_SECURE = True
