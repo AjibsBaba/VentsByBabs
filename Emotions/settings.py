1f0653596106b20ea3fbedcda7137d8303d8fea3
AjibsBaba <samuelajibade22@gmail.com>
2020-10-30 12:51:50 +0100

    added support for environment variables

--- Emotions/settings.py
+++ Emotions/settings.py
@@ -1,4 +1,6 @@
 import os
+from dotenv import load_dotenv, find_dotenv
+load_dotenv(find_dotenv())
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
