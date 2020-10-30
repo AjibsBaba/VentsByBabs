3e548f1c4a2c6b1720734f0cbccf0d458f93300b
AjibsBaba <samuelajibade22@gmail.com>
2020-10-30 12:46:18 +0100

    changed debug status

--- Emotions/settings.py
+++ Emotions/settings.py
@@ -12,7 +12,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = os.getenv('KEY')
 
 
-DEBUG = False
+DEBUG = os.getenv('STATUS')
 
 ALLOWED_HOSTS = ['vents.herokuapp.com']
 
